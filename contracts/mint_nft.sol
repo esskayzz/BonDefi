7aac3a3004aae2d37b3f9d7fda3f8406fbd3a614
Shikai <shikai55@gmail.com>
2022-01-24 16:41:31 +0800

    fix: nft minting and setting URI

--- contracts/mint_nft.sol
+++ contracts/mint_nft.sol
@@ -1,12 +1,26 @@
 //SPDX-License-Identifier: UNLICENSED
-// Recieve a NFT and mint ERC-20 bond tokens
-
-pragma solidity ^0.8.10;
+// Recieve information and mint an NFT
+pragma solidity ^0.8.0;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/utils/Counters.sol";
 
 //NFT inherits ERC721 token contract
 contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     constructor() public ERC721("BonDefi", "BDF") {}
+
+    function create(address to, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 tokenId = _tokenIds.current();
+
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
 }
