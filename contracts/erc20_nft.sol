668168bc29a6b93bc7eba9c543654a07b5415019
Xue Yao <xueyao@Xues-MacBook-Air.local>
2022-01-24 17:59:20 +0800

    can run now

--- contracts/erc20_nft.sol
+++ contracts/erc20_nft.sol
@@ -3,10 +3,7 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 //tested code
 //mints the erc-20 and gives it to the contract itself 
@@ -19,7 +16,7 @@ contract bondtoken is ERC20 {
 }
 
 //implement staking
-contract staking is bondtoken, IERC721 {
+abstract contract staking is ERC721{
 
     //replace this with our own NFT contract
     address constant nft_contract = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
